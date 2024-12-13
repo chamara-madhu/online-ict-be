f36311920b902770fbb110f57aba5f0235e87e0a
chamara-madhu <nlc.madhushanka@gmail.com>
2024-12-13 14:22:05 +0000

    adding dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,5 +1,9 @@
-const { userStats } = require("../services/dashboardService");
+const { userStats, paperStats } = require("../services/dashboardService");
 
 exports.userStats = (req, res) => {
   return userStats(res);
+};
+
+exports.paperStats = (req, res) => {
+  return paperStats(res);
 };
--- services/dashboardService.js
+++ services/dashboardService.js
@@ -1,17 +1,32 @@
-const SeasonTicket = require("../models/paper.model");
+const Paper = require("../models/paper.model");
 const User = require("../models/user.model");
 
-// exports.userStats = (res) => {
-//   User.aggregate([
-//     { $group: { _id: { role: "$role" }, count: { $sum: 1 } } },
-//     { $sort: { count: -1 } },
-//   ])
-//     .then((data) => {
-//       if (data.length > 0) {
-//         res.status(200).json(data);
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(400).json({ error: "no active users" });
-//     });
-// };
+exports.userStats = (res) => {
+  User.aggregate([
+    { $group: { _id: { role: "$role" }, count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+  ])
+    .then((data) => {
+      if (data.length > 0) {
+        res.status(200).json(data);
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({ error: "no active users" });
+    });
+};
+
+exports.paperStats = (res) => {
+  Paper.aggregate([
+    { $group: { _id: { exam: "$exam" }, count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+  ])
+    .then((data) => {
+      if (data.length > 0) {
+        res.status(200).json(data);
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({ error: err });
+    });
+};
